f842574ea23ec80c5a7fdcb190188b44507b29b1
Andrew Robbins <andydude@users.noreply.github.com>
2023-05-05 22:46:04 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='zig2lisp',
-      version='0.5',
+      version='0.6.0',
       description='Convert Zig to Lisp',
       author='Andrew Robbins',
       author_email='andjrob@gmail.com',
